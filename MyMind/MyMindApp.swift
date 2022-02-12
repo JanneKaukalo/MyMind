3fdb94123bd7fb9d61a4f2a5842f482901fae1cc
JanneKaukalo <26524906+JanneKaukalo@users.noreply.github.com>
2022-02-12 14:55:00 +0200

    Initial Commit

---
+++ MyMind/MyMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMindApp.swift
+//  MyMind
+//
+//  Created by Janne Jussila on 12.2.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
